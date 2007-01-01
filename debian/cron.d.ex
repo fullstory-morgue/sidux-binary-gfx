37c214dfa508fd1dec5128d2c40b63f09f1218d2
Stefan Lippers-Hollmann <s.l-h@gmx.de>
2007-01-01 18:09:07 +0000

    sidux-binary-gfx (1.0.0) unstable; urgency=low
      [ Andreas Weber ]
      * Initial release.
      * Update diabled.
      * nvidia-legacy driver added
      * fix for config.h removed. Not needed anymore.
    
      [ Stefan Lippers-Hollmann ]
      * adapt package version for native packaging.
      * remove dh_make templates from debian/.
      * move detect_nvidia to /usr/share/sidux-binary-gfx/, remove auto-update
        completely.
      * strip *.sh suffix.
      * fix typo.
    
     -- Stefan Lippers-Hollmann <s.l-h@gmx.de>  Mon,  1 Jan 2007 19:07:24 +0100

--- debian/cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the sidux-binary-gfx package
-#
-0 4	* * *	root	sidux-binary-gfx_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(sidux-binary-gfx):needs="X11|text|vc|wm" section="Apps/see-menu-manual"\
-  title="sidux-binary-gfx" command="/usr/bin/sidux-binary-gfx"
